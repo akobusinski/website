457e112c00ab5d2d535335f0f9491610e2b34008
Antoni Kobusinski <60139174+akobusinski@users.noreply.github.com>
2023-12-31 13:26:58 +0100

    Use lifespan events instead of on_startup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi_cache.backends.inmemory import InMemoryBackend
 from dotenv import load_dotenv; load_dotenv()
 from fastapi.responses import JSONResponse
+from contextlib import asynccontextmanager
 from fastapi_cache import FastAPICache
 from fastapi import Request
 import dynamic_router
@@ -8,7 +9,12 @@ import uvicorn.main
 import logging
 import fastapi
 
-app = fastapi.FastAPI(description="Backend", version="1.0.0")
+@asynccontextmanager
+async def lifespan(app: fastapi.FastAPI):
+    FastAPICache.init(InMemoryBackend()) # type: ignore   "Type of "x" is partially unknown" 🤓
+    yield
+
+app = fastapi.FastAPI(description="Backend", version="1.0.0", lifespan=lifespan)
 router = dynamic_router.DynamicRouter()
 
 router.logger.setLevel(logging.WARNING)
@@ -20,10 +26,6 @@ app.include_router(router.get_router())
 @app.exception_handler(Exception)
 async def exception_handler(request: Request, exc: Exception):
     return JSONResponse(status_code=500, content={"success": False, "message": "Internal server error"})
-
-@app.on_event("startup")
-async def startup():
-    FastAPICache.init(InMemoryBackend()) # type: ignore   "Type of "x" is partially unknown" 🤓
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080) # type: ignore   "Type of "x" is partially unknown" 🤓
