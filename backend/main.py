a454568ab5e9e213db4bbc9917fb32941c5a3066
Antoni Kobusinski <60139174+akobusinski@users.noreply.github.com>
2023-12-31 12:10:24 +0100

    Replace redis with in memory caching

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,12 @@
-from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.backends.inmemory import InMemoryBackend
 from dotenv import load_dotenv; load_dotenv()
 from fastapi.responses import JSONResponse
 from fastapi_cache import FastAPICache
-from redis import asyncio as aioredis
 from fastapi import Request
 import dynamic_router
 import uvicorn.main
 import logging
 import fastapi
-import os
 
 app = fastapi.FastAPI(description="Backend", version="1.0.0")
 router = dynamic_router.DynamicRouter()
@@ -25,8 +23,7 @@ async def exception_handler(request: Request, exc: Exception):
 
 @app.on_event("startup")
 async def startup():
-    redis = aioredis.from_url(os.getenv("REDIS_URL")) # type: ignore   "Type of "x" is partially unknown" 🤓
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache") # type: ignore   "Type of "x" is partially unknown" 🤓
+    FastAPICache.init(InMemoryBackend()) # type: ignore   "Type of "x" is partially unknown" 🤓
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080) # type: ignore   "Type of "x" is partially unknown" 🤓
